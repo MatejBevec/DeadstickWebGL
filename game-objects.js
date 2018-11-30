e10d1f1048edde2ab6b04db1b44bc22701c85d13
MatejBevec <40042371+MatejBevec@users.noreply.github.com>
2018-11-30 11:47:33 +0100

    Added Avioncl

--- game-objects.js
+++ game-objects.js
@@ -13,7 +13,6 @@ function ExampleObject(label){
 	this.texture = initTexture(gl, './assets/sand.jpg');
 
 	this.doTranslate([1.0,0,0]);
-	console.log(this);
 }
 ExampleObject.prototype = Object.create(VisibleObject.prototype);
 ExampleObject.prototype.onTick = function(){
@@ -62,10 +61,117 @@ FlyingBoi.prototype.onTick = function(){
 		this.doRotate(0.02, [0,0,1]);
 	}
 
-	var dir = vec3.fromValues(0,0.1,0);
+	var dir = vec3.fromValues(0,0.2,0);
 	vec3.transformMat4(dir,dir,this.rotate);
 
 	if(Input[32]){
 		this.doTranslate(dir);
 	}
+}
+
+//write new game objects here
+
+//to copy an object call clone(src), don't use new ...
+function Avioncl(label){
+	VisibleObject.call(this, label);
+
+	//write any initialization code here, or do it in main.js
+	//a global gl context object "gl" is taken for granted - bad practice
+	mExample = new Model("mExample");
+	mExample.openUrl('./assets/avioncl.obj', 'obj');
+	//mExample.center();
+	this.model = mExample;
+	//this.texture = initTexture(gl, './assets/kocka.png');
+	this.doRotate(Math.PI,[0,1,0]);
+	this.doScale([0.2,0.2,0.2]);
+	console.log(this);
+
+
+	this.minHitrost = 1;
+	this.maxHitrost = 7;
+	this.hitrost = this.minHitrost;
+	this.krila = [-1,0,0];
+	this.trup = [0,0,-1];
+	this.normala = [0,1,0];
+
+}
+
+Avioncl.prototype = Object.create(VisibleObject.prototype);
+Avioncl.prototype.onTick = function(){
+	VisibleObject.prototype.onTick.call(this);
+
+	//object behavior every tick goes here
+
+		//PREMALO SPEEDA
+		/*
+		if(this.hitrost<this.minHitrost){
+			if(this.normala[1]>0){
+				this.doRotate(0.06-(this.hitrost*0.03),[1,0,0]);
+			}
+			else{
+				this.doRotate(0.06-(this.hitrost*0.03),[-1,0,0]);
+			}
+		}*/
+
+		//POGON
+		if(Input[32]&&this.hitrost<this.maxHitrost){  //space
+			this.hitrost*=1.01;
+		}
+		else if(this.hitrost>this.minHitrost){
+			this.hitrost/=1.01;
+		}
+
+		// OBNAŠANJE AVIONCLA ZARADI TEŽE
+		if(this.trup[1]>0){
+			this.hitrost*=1+(this.trup[1]*0.03);
+		}
+		if(this.trup[1]<0&&this.hitrost>this.minHitrost-1){
+			this.hitrost/=1+Math.abs(this.trup[1])*0.01;
+		}
+
+		//OBAŠANJE AVIONCLA ZARADI VZGONA
+		if(this.krila[1]>0){
+			this.doRotate(0.01*this.krila[1],[0,1,0]);
+			this.doRotate(0.01*this.krila[1],[-1,0,0]);
+		}
+		if(this.krila[1]<0){
+			this.doRotate(0.01*this.krila[1],[0,1,0]);
+			this.doRotate(0.01*this.krila[1],[1,0,0]);
+		}
+
+		/*TALE JE KR UREDU. tu je tud OBNAŠANJE ZARAD VZGONA avioncl ne gre tuk dol
+		if(this.krila[1]>0){
+			this.doRotate(0.02*(1-this.krila[1]),[0,1,0]);
+			this.doRotate(0.01*this.krila[1],[-1,0,0]);
+		}
+		if(this.krila[1]<0){
+			this.doRotate(0.02*(-1-this.krila[1]),[0,1,0]);
+			this.doRotate(0.01*this.krila[1],[1,0,0]);
+		}
+		*/
+
+		if(Input[87]){ //W
+			this.doRotate(0.02,[1,0,0]);
+		}
+		if(Input[83]){  //S
+			this.doRotate(0.02,[-1,0,0]);
+		}
+		if(Input[65]){  //A
+			this.doRotate(0.03,[0,0,-1]);
+		}
+		if(Input[68]){   //D
+			this.doRotate(0.03,[0,0,1]);
+		}
+
+		this.krila = [-1,0,0];
+		this.trup = [0,0,-1];
+		this.normala = [0,1,0];
+		var pogon = vec3.fromValues(0,0,0.1);
+		vec3.transformMat4(pogon,pogon,this.rotate);
+		vec3.transformMat4(this.krila,this.krila,this.rotate);
+		vec3.transformMat4(this.trup,this.trup,this.rotate);
+		vec3.transformMat4(this.normala,this.normala,this.rotate);
+		vec3.scale(pogon,pogon,this.hitrost);
+		this.doTranslate(pogon);
+	
 }
--- main.js
+++ main.js
@@ -23,9 +23,10 @@ function start(){
 
 	//initializing the camera and the shader
 	const camera = new CameraObject("cam1", 55, CameraObject.prototype.glRatio(gl), 0.01, 10000.0);
-	camera.doTranslate([0,-4,-1]);
-	camera.doRotate(Math.PI/2 +0.1, [1,0,0]);
-	camera.doRotate(Math.PI, [0,0,1]);
+	camera.doTranslate([0,3,-15]);
+	camera.doRotate(Math.PI, [0,1,0]);
+	//camera.doRotate(Math.PI/2 + 0.1, [1,0,0]);
+	//camera.doRotate(Math.PI, [0,0,1]);
 	//camera.doTranslate([0,0,10]);
 	const programInfo = initShaderProgram(gl, shader1.v, shader1.f, shader1.info);
 
@@ -45,7 +46,13 @@ function start(){
 	oSurface.texture = initTexture(gl, './assets/sand.jpg');
 	console.log(oSurface.texture);
 	//oSurface.doScale([0.5,0.5,0.5]);
-	oSurface.doTranslate([0,-2,0]);
+	oSurface.doTranslate([0,-10,0]);
+	oSurface.doScale([10,10,10]);
+
+	var oAvioncl = new Avioncl("oAvioncl");
+	oAvioncl.doScale([0.1,0.1,0.1]);
+	oAvioncl.doRotate(Math.PI, [0,1,0]);
+	//oAvioncl.doRotate();
 
 	var exampleObject = new ExampleObject("exampleObject");
 	oSurface.texture = exampleObject.texture;
@@ -57,28 +64,12 @@ function start(){
 	var scene = new Scene();
 	//exampleObject.attach(obj2);
 	exampleObject.attach(obj2);
-	oBoi.attach(camera);
+	oAvioncl.attach(camera);
+	scene.root.attach(oAvioncl);
 	scene.root.attach(exampleObject);
 	scene.root.attach(oSurface);
 	scene.root.attach(oBoi);
 	//console.log(scene);
-
-	//some tests
-	var m1 = mat4.create();
-	var m2 = mat4.create();
-	mat4.rotate(m1, m1, 1, [1,0.5,0]);
-	mat4.translate(m1, m1, [1,2,3]);
-	mat4.rotate(m1, m1, 0.5, [0,0,1]);
-	mat4.translate(m1, m1, [3,-2,5]);
-	var rot = quat.create();
-	var tr = vec3.create();
-	mat4.getRotation(rot, m1);
-	mat4.getTranslation(tr, m1);
-	console.log(rot,tr);
-	mat4.fromRotationTranslation(m2, rot, tr);
-
-	console.log(mat4.equals(m1,m2));
-	console.log(m1,m2);
 
 	//GAME LOOP
 	var then;
